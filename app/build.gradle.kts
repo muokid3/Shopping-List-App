6f35cc030213c1aff9b6fe4d2002b564f0630abf
Dennis Muoki <muokid3@gmail.com>
2024-06-14 11:10:44 +0300

    added maps compose, retrofit and viewmodel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.play.services.maps)
+    implementation(libs.maps.compose)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
